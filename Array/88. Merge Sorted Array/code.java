a7d3647712a34b1813b3dbd631cbd18f0dd0b163
Tanmay Gupta <96345346+Tanmaygupta8503@users.noreply.github.com>
2024-07-18 14:59:58 +0530

    Create code.java

---
+++ Array/88. Merge Sorted Array/code.java
@@ -0,0 +1,39 @@
+class Solution1 {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        for(int i = 0 ; i < n ; i ++) {
+            int val = nums2[i];
+            int j = (m + i) - 1;
+            while(j >= 0 && nums1[j] > val) {
+                nums1[j + 1] = nums1[j];
+                j --;
+            }
+            nums1[j + 1] = val;
+        }
+    }
+}
+
+
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int zero = nums1.length - 1;
+        int one = m - 1;
+        while(one >= 0) {
+            nums1[zero --] = nums1[one --];
+        }
+        int i = nums1.length-m , j = 0;
+        int ptr = 0;
+        while(i < nums1.length && j < n) {
+            if(nums1[i] < nums2[j]) {
+                nums1[ptr ++] = nums1[i ++];
+            }else{
+                nums1[ptr ++] = nums2[j ++];
+            }
+        } 
+        while(i < nums1.length) {
+            nums1[ptr ++] = nums1[i ++];
+        }
+        while(j < n) {
+            nums1[ptr ++] = nums2[j ++];
+        }
+    }
+}
