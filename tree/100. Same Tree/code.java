5f7f3b6cf967d818402b5a731459aa2968689de8
Tanmay Gupta <96345346+Tanmaygupta8503@users.noreply.github.com>
2024-07-25 16:23:48 +0530

    Create code.java

---
+++ tree/100. Same Tree/code.java
@@ -0,0 +1,8 @@
+class Solution {
+    boolean isSameTree(TreeNode root1 , TreeNode root2) {
+        if(root1 == null && root2 == null) return true;
+        if(root1 == null || root2 == null) return false;
+        if(root1.val != root2.val) return false;
+        return isSameTree(root1.left , root2.left) && isSameTree(root1.right , root2.right); 
+    }
+}
